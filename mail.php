8b7383b262d185b57f9860d350e13b1fe76eec7a
BarabanEvgeniy <132493593+BarabanEvgeniy@users.noreply.github.com>
2023-05-05 13:44:07 +0300

    Фдд кусутпу

---
+++ mail.php
@@ -0,0 +1,12 @@
+<?php
+$to = "baraban.jenia@icloud.com"; // адрес куда отправлять письмо
+$subject = "Новый заказ на Напальчник"; // заголовок письма
+$redirect = "/thanks.html"; // адрес страницы, на которую нужно перейти после отправки письма
+
+foreach($_POST as $key => $value)
+{ $fields .= $key.": ".$value." \r\n"; }
+$message = $subject." \r\n".$fields; 
+$headers  = "Content-type:  text/plain; charset=utf-8 \r\n"; 
+mail($to, $subject, $message, $headers);
+header('Location: '.$redirect);
+?>
