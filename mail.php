f07658f25d248fc56625f55a0644a8307d85888f
BarabanEvgeniy <132493593+BarabanEvgeniy@users.noreply.github.com>
2023-05-05 17:50:59 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,9 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Credentials: true");
-header("Access-Control-Max-Age: 1000");
-header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
-header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
 $to = "baraban.jenia@icloud.com"; // адрес куда отправлять письмо
 $subject = "Новый заказ на Напальчник"; // заголовок письма
 $redirect = "/thanks.html"; // адрес страницы, на которую нужно перейти после отправки письма
@@ -13,5 +8,10 @@ foreach($_POST as $key => $value)
 $message = $subject." \r\n".$fields; 
 $headers  = "Content-type:  text/plain; charset=utf-8 \r\n"; 
 mail($to, $subject, $message, $headers);
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
 header('Location: '.$redirect);
 ?>
